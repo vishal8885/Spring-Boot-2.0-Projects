01e55a0231137c18bb86f4d01bfe151ff47f73b0
Vishal_Punzuri <vishalmudhiraj71@gmail.com>
2020-12-31 18:06:21 +0530

    Movie rating system initial setup

---
+++ springboot2movierating/src/main/kotlin/com/learning/springboot2movierating/Springboot2movieratingApplication.kt
@@ -0,0 +1,11 @@
+package com.learning.springboot2movierating
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Springboot2movieratingApplication
+
+fun main(args: Array<String>) {
+	runApplication<Springboot2movieratingApplication>(*args)
+}
---
+++ springboot2movierating/src/test/kotlin/com/learning/springboot2movierating/Springboot2movieratingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.learning.springboot2movierating
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Springboot2movieratingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
